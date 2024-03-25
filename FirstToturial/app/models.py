441894466d42bd02c989ec63b446a2f282497746
acontius <152691439+acontius@users.noreply.github.com>
2024-03-25 13:46:39 +0330

    Q/A added and its SQL(checked by SQLite online)

---
+++ FirstToturial/app/models.py
@@ -0,0 +1,12 @@
+#To save any thing we want in the database of the current app
+from django.db import models
+
+class Question(models.Model):
+    text = models.CharField(max_length = 300)
+    date = models.DateField()
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question , on_delete=models.CASCADE )
+    Choice = models.CharField(max_length = 300)
+    votes = models.IntegerField(default = 0)
